65799a59ac5334626415bc5e1ea7d507386ec83a
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-04-13 10:35:35 +0300

    changed package json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ghPages({
+      // ghPages plugin options go here
+      // for example:
+      domain: 'oleksandr-maly.github.io/CRM-Passenger-transportation//'
+      // or
+      // cname: '<custom-domain-name>.com'
+    })
+  ],
+  build: {
+    base: '/CRM-Passenger-transportation/'
+  },
 })
