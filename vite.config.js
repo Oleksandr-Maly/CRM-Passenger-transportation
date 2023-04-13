40008c64f24fc3f25089648290162f0fc5d16968
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-04-13 10:55:35 +0300

    new commit

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,14 @@ dotenv.config({ path: '.env.local' });
 export default defineConfig({
   plugins: [
     react(),
-    ghPages({
-      // ghPages plugin options go here
-      // for example:
-      domain: 'oleksandr-maly.github.io/CRM-Passenger-transportation//'
-      // or
-      // cname: '<custom-domain-name>.com'
-    })
+    // ghPages({
+    //   // ghPages plugin options go here
+    //   // for example:
+    //   domain: 'oleksandr-maly.github.io/CRM-Passenger-transportation'
+
+    //   // or
+    //   // cname: '<custom-domain-name>.com'
+    // })
   ],
   build: {
     base: '/CRM-Passenger-transportation/'
