5870375ac887ad0b4d43d1c5c80a66ec7461cf99
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-04-13 10:16:51 +0300

    prepeare to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import ghPages from 'vite-plugin-gh-pages';
 
 dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
 })
